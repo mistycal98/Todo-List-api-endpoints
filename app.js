d701ce9b086c368cb6489f4628e0216258ffaf08
mystical98 <tushar.mistry@raweng.com>
2021-01-08 11:20:28 +0530

    Added cors policy

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 // Config file import path
 dotenv.config({ path: "./config.env" });
 
 // Create Server
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Import Routes
@@ -18,35 +20,30 @@ app.use("/todos", todoRoutes);
 
 //Default route (Home Page)
 app.get("/", (req, res) => {
-  res.send("we are on home page");
+	res.send("we are on home page");
 });
 
 //If Route not found
-app.get('*',(req,res) => {
-  res.send('<h1>404 Page not Found</h1>');
+app.get("*", (req, res) => {
+	res.send("<h1>404 Page not Found</h1>");
 });
 
 // DB connection
 try {
-  mongoose.connect(
-    process.env.DB_CONNECTION,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    () => {
-      console.log("Connected to DB!");
-    }
-  );
+	mongoose.connect(
+		process.env.DB_CONNECTION,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		},
+		() => {
+			console.log("Connected to DB!");
+		}
+	);
 } catch (error) {
-  console.log('Error Occured', error);
+	console.log("Error Occured", error);
 }
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on http://localhost:${process.env.PORT}`);
+	console.log(`Server started on http://localhost:${process.env.PORT}`);
 });
-
-
-
-
-
